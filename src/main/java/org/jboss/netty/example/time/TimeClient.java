1a0cfd5c6204018feaab3a4995f571c44b0f5e28
qianjinjiu <2812025816@qq.com>
2016-03-15 14:37:04 +0800

    add netty example TimeServer

---
+++ src/main/java/org/jboss/netty/example/time/TimeClient.java
@@ -0,0 +1,45 @@
+package org.jboss.netty.example.time;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelEvent;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelHandler;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.ChannelSink;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+
+public class TimeClient {
+
+	public static void main(String[] args) {
+		String host = args[0];
+		int port = Integer.parseInt(args[1]);
+		
+		ChannelFactory factory = new NioClientSocketChannelFactory(
+				Executors.newCachedThreadPool(),
+				Executors.newCachedThreadPool());
+		
+		ClientBootstrap bootstrap = new ClientBootstrap(factory);
+		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+			
+			@Override
+			public ChannelPipeline getPipeline() throws Exception {
+				return Channels.pipeline(new TimeClientHandler());
+			}
+		});
+		
+		bootstrap.setOption("child.tcpNoDelay", true);
+		bootstrap.setOption("child.keepAlive", true);
+		bootstrap.connect(new InetSocketAddress(host, port));
+	}
+
+}
---
+++ src/main/java/org/jboss/netty/example/time/TimeServerHandler.java
@@ -0,0 +1,37 @@
+package org.jboss.netty.example.time;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+public class TimeServerHandler extends SimpleChannelHandler {
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
+			throws Exception {
+		e.getCause().printStackTrace();
+		e.getChannel().close();
+	}
+
+	@Override
+	public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e)
+			throws Exception {
+		ChannelBuffer time = ChannelBuffers.buffer(4);
+		time.writeInt((int) (System.currentTimeMillis() / 1000L) );
+		ChannelFuture f = e.getChannel().write(time);
+		f.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) throws Exception {
+				future.getChannel().close();
+			}
+		});
+	}
+
+
+	
+}
